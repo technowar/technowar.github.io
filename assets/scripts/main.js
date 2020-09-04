26e633352266cc0c93c3559898ebe34d6374da1c
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2020-09-04 18:43:12 +0800

    update css and js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,7 @@
 (function() {
-  function init () {
-    console.log('init');
-  }
-
   window.addEventListener('load', function () {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('../../sw.js');
-    }
-
-    if (document.readyState === 'complete') {
-      init();
     }
   });
 }());
